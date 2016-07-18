3c6913c5cdb8f241b1ae43bf48501f8ba8bf8a82
lusor83 <lusor83@gmail.com>
2016-07-18 17:58:12 +0200

    Update scratch.js

--- scratch.js
+++ scratch.js
@@ -1,39 +1,12 @@
-$(document).ready(function(){
-$("#pagewrapper").css({'background':'url(http://i.imgur.com/ra8F49A.jpg)',"background-size":"100% auto","background-repeat": "repeat-y"}).css('perspective', '800px');
-$("#content").css({'transform-style': 'preserve-3d', 'transition': 'transform 0.5s', 'backface-visibility': 'hidden'});
-$("#topnav").css("z-index", "9999999");
-$("#djangobbindex").css({'background':'rgba(255, 255, 255, 0.8)', 'padding':'10px', 'border-radius':'10px'});
+function installHelloWorld() { (function(ext) { 
+ // Cleanup function when the extension is unloaded 
+ ext._shutdown = function() {}; 
+ // Status reporting code // Use this to report missing hardware, plugin or unsupported browser 
+ ext._getStatus = function() { return {status: 2, msg: 'Installed'}; };
+ var descriptor = { blocks: [ [' ', 'hello world', 'helloWorld'] ] }; 
+ //The Hello world function 
+ ext.helloWorld = function(){ alert('Hello World!') }; 
+ ScratchExtensions.register('Hello World', descriptor, ext); })({}); 
  
-var t, l = (new Date()).getTime(), scrolling = false;
-initialAmt = 0;
-$(window).scroll(function(){
-    $("#pagewrapper").css('background-position-y', ($(document).scrollTop()*10/11)+'px');
-    $("#content").css('transform-origin', (($(document).width()/2))+"px "+($(document).scrollTop()+$(window).height()/2)+"px");
-    diff = Math.abs($(document).scrollTop()-initialAmt);
-    if(diff>600)$("#content").css('transform', 'rotateX(-5deg) translateZ(-50px) translateY(50px)');
-    var now = (new Date()).getTime();
-    if(now - l > 400 && !scrolling ){
-        $(this).trigger('scrollStart');
-        l = now;
-    }
-    clearTimeout(t);
-    t = setTimeout(function(){
-        if (scrolling)
-            $(window).trigger('scrollEnd');
-    }, 300);
-});
- 
-$(window).bind('scrollStart', function(){
-    scrolling = true;
-    initialAmt = $(document).scrollTop();
-});
- 
-$(window).bind('scrollEnd', function(){
-    scrolling = false;
-    initialAmt = $(document).scrollTop();
-    $("#content").css('transform', 'none');
-});
-window.onbeforeunload=function(){
-   $("#content").css('transform', 'translateZ(-50px)').animate({'opacity':'0'}, 700);
-};
-});
+}
+installHelloWorld();
