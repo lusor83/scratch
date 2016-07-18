bd408521bb4a801ba6e272fcebb0f7a2ac3b6c01
Luigi Sorrentino <lusor83@gmail.com>
2016-07-18 17:24:04 +0200

    scratch

--- Scrivania/scratch.js
+++ Scrivania/scratch.js
@@ -1,25 +1,7 @@
-(function(ext) {
-    // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
+function installHelloWorld() { (function(ext) { // Cleanup function when the extension is unloaded 
+ext._shutdown = function() {}; // Status reporting code // Use this to report missing hardware, plugin or unsupported browser 
+ext._getStatus = function() { return {status: 2, msg: 'Installed'}; }; var descriptor = { blocks: [ [' ', 'hello world', 'helloWorld'] ] }; 
+//The Hello world 
+function ext.helloWorld = function(){ alert('Hello World!') }; ScratchExtensions.register('Hello World', descriptor, ext); })({}); }
 
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
-    };
-
-    ext.my_first_block = function() {
-        // Code that gets executed when the block is run
-    };
-
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-            // Block type, block name, function name
-            [' ', 'my first block', 'my_first_block'],
-        ]
-    };
-
-    // Register the extension
-    ScratchExtensions.register('My first extension', descriptor, ext);
-})({});
+installHelloWorld();
